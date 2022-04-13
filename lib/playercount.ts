383b08507cbedf3163ecd660351c1741c77b3d43
Heimatmeister <103588023+Heimatmeister@users.noreply.github.com>
2022-04-13 11:47:44 -0400

    Update playercount.ts

--- lib/playercount.ts
+++ lib/playercount.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function getOnlinePlayers(): Promise<string> {
-    const json = (await axios.get("https://api.mcsrvstat.us/2/6b6t.org")).data
+    const json = (await axios.get("https://api.mcsrvstat.us/2/2b2t.de.com")).data
 
     const online = json.players.online
     const max = json.players.max
