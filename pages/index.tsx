9688106f4cd1db76870de0f5380c90a242a8aeec
Heimatmeister <103588023+Heimatmeister@users.noreply.github.com>
2022-04-13 11:45:38 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,37 +6,26 @@ const Home: NextPage = () => {
         <Layout>
             <h2>About</h2>
             <ul className="mb-2">
-                <li>IP: 6b6t.org</li>
+                <li>IP: 2b2t.com.de & 2b2t.de.com</li>
                 <li>Versions: 1.8.x to 1.18.x (1.12.2 Recommended)</li>
-                <li>CPU: AMD Ryzen 9 5950X</li>
-                <li>RAM: 128GB DDR4</li>
-                <li>STORAGE: 2x2TB NVMe SSD Gen 4.0</li>
-                <li>DDOS Protection: Path.net</li>
-                <li>6b6t is a non-vanilla cracked anarchy server with no rules which means everything is
-                    allowed and you can not get punished (No bans or mutes or kicks).
+                <li>CPU: AMD Ryzen 7 3700X</li>
+                <li>RAM: 64GB DDR4</li>
+                <li>STORAGE: 2x8TB HDDs</li>
+                <li>DDOS Protection: cosmicguard</li>
+                <li>2builders2tools Germany is a minecraft server that promises its players vanilla survival gameplay with PvP, 
+                    a neverending world and no rules. The IP is 2b2t.de.com or 2b2t.com.de. It has been on the same world since April 2022..
                 </li>
             </ul>
 
-            <h2>Discord</h2>
-            <p className="pb-2">
-                Join the 6b6t.org Official Discord Server to keep up with the latest updates with the
-                server: <a href="https://dsc.gg/6b6t.org" target="_blank"
-                           className="link"
-                           rel="noreferrer">https://dsc.gg/6b6t.org</a>
-            </p>
-
             <h2>Popular questions</h2>
 
-            <h3>What is 6b6t?</h3>
+            <h3>What is 2b2t germany?</h3>
             <p>
-                6b6t is a Minecraft Anarchy Server, that means there are no rules. It&apos;s one of the
-                few
-                cracked (also known as offline mode) anarchy servers which allows everyone to play. The
-                server is semi-vanilla, it has commands like /tpa, /home and /hat. 6b6t started January
-                16 2022 and the map will never reset.
+                2builders2tools Germany is a minecraft server that promises its players vanilla survival gameplay with PvP, 
+                    a neverending world and no rules. The IP is 2b2t.de.com or 2b2t.com.de. It has been on the same world since April 2022.
             </p>
 
-            <h3>What is 6b6t seed?</h3>
+            <h3>What is 2b2t Germany seed?</h3>
             <p>6b6t seed is the same as 2b2t seed: -4172144997902289642</p>
 
             <h3>Is hacking allowed?</h3>
@@ -44,8 +33,7 @@ const Home: NextPage = () => {
 
             <h3>Are lag machines allowed?</h3>
             <p>
-                Yes, 6b6t admins already patched most of the lag machines and if you help us find a
-                working one, we will reward you.
+                Yes, Heimatmeister already patched most of the lag machines.
             </p>
         </Layout>
     )
